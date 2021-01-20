31e1548ea8314c04e1614bdb96671fa21ba1e3ab
Munima <Munima@10.0.0.141>
2021-01-20 16:55:18 -0700

    Lab2 skeleton added

---
+++ src/mru/lab1/application/GeometryDemo.java
@@ -0,0 +1,17 @@
+package mru.lab1.application;
+
+import mru.lab1.project.Geometry;
+
+public class GeometryDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Starting your program");
+		//Creating an instance of the Geometry class
+		Geometry geo = new Geometry();
+		
+		//Calling the runApplication method
+		geo.runApplication();
+
+	}
+
+}
